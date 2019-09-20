689490b87e3e50fa55497d9a5226347a9b45b2e0
Felipe Velandia <fvelandia695@gmail.com>
2019-09-20 09:52:40 -0500

    Add files via upload

--- prim.py
+++ prim.py
@@ -1,3 +1,58 @@
+def Prim ():
+    global iguales,op,x,marcados, marcados1, padres, m, temp, temp2, minimos, x
+    print("Op: "+str(op)+", Iguales: "+str(iguales))
+    iguales=False
+    while op !=0:
+        temp = 99
+        print("Padres: "+str(padres))
+        print("Minimos: "+str(minimos))
+        for i in range(9):
+            if padres[i] == 0:
+                if m[x][i] != 0 and marcados[i] != 1:
+                    if minimos[i] > m[x][i]:
+                        minimos[i] = m[x][i]
+                        padres[i] = x+1
+                    if m[x][i] < temp:
+                        temp = m[x][i]
+                        temp2 = i
+                        print("break")
+                        break
+        x = temp2
+        print
+        print(marcados1)
+        print
+        if marcados[temp2]==0:
+            marcados[temp2] = 1
+        else:
+            marcados[temp2]=2
+            
+        '''
+    
+        '''
+        print
+        print(marcados1)
+        print
+        print(marcados)
+        print
+        print(padres)
+        print
+        
+        for i in range (9):
+            if(marcados[i]==marcados1[i]):
+                iguales=True
+            else:
+                iguales=False
+                break
+        
+        
+        op = 0
+        for i in range(9):
+            if marcados[i] == 0 and iguales==False:
+                op = 1    
+        marcados1=list(marcados)
+        
+    return iguales
+
 print("PROGRAMA ALGORITMO DE PRIM")
 print
 
@@ -36,24 +91,18 @@ m[8][2] = 5
 m[8][6] = 2
 m[8][7] = 7
 
-x=0;  op=1; marcados[0]=1; minimos[0]=0; marcados1=list(marcados)
+x=0;  op=1; marcados[0]=1; minimos[0]=0; marcados1=list(marcados); iguales=False
 
-print(padres)
-print()
-print(marcados)
-print()
-print(minimos)
-print()
 
 
 for i in m:
     print(i)
-
+'''
 while op !=0:
     temp = 99
     for i in range(9):
         if padres[i] == 0:
-            if m[x][i] != 0 and marcados[i] == 0:
+            if m[x][i] != 0 and marcados[i] != 1:
                 if minimos[i] > m[x][i]:
                     minimos[i] = m[x][i]
                     padres[i] = x+1
@@ -69,9 +118,6 @@ while op !=0:
     else:
         marcados[temp2]=2
         
-    '''
-
-    '''
     print()
     print(marcados1)
     print()
@@ -95,31 +141,34 @@ while op !=0:
             op = 1    
     marcados1=list(marcados)
     print("Op: "+str(op))
-
-                
-print("Marcados final: "+str(marcados))
-print()
-print("Padres final: "+str(padres))
-print()
-  
-for i in range (9):
-    menor=100
-    for j in range (9):
-        if m[i][j]<menor and m[i][j]!=0:
-            menor=m[i][j]
-    minimos[i]=menor
-    
-for i in range (9):
-    if marcados[i] > 1:
+    '''
+while(Prim()):
+    minimos1=[99]*9
+    for i in range (9):
+        menor=100
         for j in range (9):
-            if m[i][j] == minimos[i] :
-                padres[i]=j+1
-                marcados[i] = 1
+            if m[i][j]<menor and m[i][j]!=0:
+                menor=m[i][j]
+        minimos1[i]=menor
+        
+    for i in range (9):
+        if marcados[i] >1:
+            for j in range (9):
+                if m[i][j] == minimos1[i] and padres[j]!=i+1:
+                    padres[i]=j+1
+                    marcados[i] = 1
+                    marcados[x]=1
+                    break
+            break
+    print
+    print("Correccion: "+str(marcados))
+    print
+    print("Padres: "+str(padres))
                 
 print("Marcados final: "+str(marcados))
-print()
+print
 print("Padres final: "+str(padres))
-print()
+print
     
 for i in m:
     print(i)
